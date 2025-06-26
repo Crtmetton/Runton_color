81609f58b0107b1dda1f06ace3a7b4e6b08dc6a0
noxa38 <lfnoxa38@gmail.com>
2025-06-26 15:16:53 +0200

    Vérification du servlet acceuil et ajout des images ainsi que des quelque modification visuel

--- update-imports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Trouver tous les fichiers .java
-echo "Remplacer les imports jakarta par javax..."
-find src/main/java -name "*.java" -type f -exec sed -i '' 's/import jakarta\.servlet/import javax.servlet/g' {} \;
-find src/main/java -name "*.java" -type f -exec sed -i '' 's/import jakarta\.mail/import javax.mail/g' {} \;
-
-echo "Mise à jour terminée" 
